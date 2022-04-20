4392435c1d8a0ffad69e7f81b1ee1bd612e08f7c
MARTIN <MP502465>
2022-04-20 14:55:35 +0200

    aaaaaaaaa

--- include/Produit.h
+++ include/Produit.h
@@ -1,7 +1,6 @@
-#include <stdio.h>
 #ifndef PRODUIT_H_INCLUDED
 #define PRODUIT_H_INCLUDED
-
+#include <stdio.h>
 typedef struct {
 unsigned int code;
 float prix ;
@@ -12,5 +11,5 @@ Produit creerProduit(unsigned int code, float prix, char* nom);
 Produit saisirProduit();
 void afficherProduit(Produit* p);
 unsigned char lireUnProduit(FILE* fichier,Produit* produit);
-void afficherProduit(Produit p);
+//void afficherProduit(Produit p);
 #endif // PRODUIT_H_INCLUDED
